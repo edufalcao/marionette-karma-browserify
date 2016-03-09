832bc26d5cc88f024c44d0dba899ee96985aec05
Eduardo Falcão Lima <edu.falcao@gmail.com>
2016-03-08 22:14:49 -0300

    Fix livereload. Add browserify and jquery modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,46 +1,78 @@
 module.exports = function(grunt) {
-  grunt.loadNpmTasks('grunt-contrib-copy');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks("grunt-html-build");
+  grunt.loadNpmTasks("grunt-browserify");
+  grunt.loadNpmTasks("grunt-contrib-copy");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-contrib-connect");
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
+
+    htmlbuild: {
+      dev: {
+        src: "app/index.html",
+        dest: "target/"  
+      },
+
+      release: {
+        //configure release
+      }
+    },
+
+    browserify: {
+      dev: {
+        files: {
+          "target/app.js": ["app/js/app.js"]
+        },
+        options: {
+          debug: true,
+          watch: true
+          //transform: [""]
+        }
+      }
+    },
 
     copy: {
       main: {
         files: [
-          {expand:true, cwd: "app/", src: ["index.html"], dest: "target/"}
+          // copy assets
         ]
       }
     },
 
     watch: {
-      app: {
-        files: ["app/**/*"],
-        tasks: ["copy"],
-        options: {
-          livereload: true
-        }
+      options: {
+        livereload: true,
+        spawn: false
+      },
+
+      index: {
+        files: ["app/index.html"],
+        tasks: ["htmlbuild:dev"]
+      },
+
+      js: {
+        files: ["app/**/*.js"]
       }
     },
 
     connect: {
       server: {
         options: {
-          port: 8000,
+          port: 3001,
           base: "target/",
-          // Change this to '0.0.0.0' to access the server from outside
-          hostname: 'localhost'
+          hostname: "localhost", // Change this to 0.0.0.0 to access the server from outside
+          open: true
         }
       }
     }
   });
   
-  grunt.event.on('watch', function(action, filepath, target) {
-    grunt.log.writeln(target + ': ' + filepath + ' has ' + action);
+  grunt.event.on("watch", function(action, filepath, target) {
+    grunt.log.writeln(target + ": " + filepath + " has " + action);
   });
 
   // Default task(s).
-  grunt.registerTask('default', ["copy", "connect", "watch"]);
+  grunt.registerTask("default", ["htmlbuild:dev", "browserify:dev", "copy", "connect", "watch"]);
 };
